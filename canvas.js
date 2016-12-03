19e3c836d938eb6e5c48b695f92a8950d7e1ad9d
Patrick Simonian <patricksimonian@gmail.com>
2016-12-02 20:10:04 -0800

    have ball moving in a box now

--- canvas.js
+++ canvas.js
@@ -1,31 +1,62 @@
-var canvas = document.getElementById('game');
 
-var ctx = canvas.getContext("2d");
-canvas.height = 500;
-canvas.width = 500;
+(function initialize() {
+  var canvas = document.getElementById('game');
+  canvas.height = 500;
+  canvas.width = 500;
+  var ctx = canvas.getContext("2d");
+  var x = 150;
+  var y = 150;
+  var dx = 2;
+  var dy = 4;
+  var WIDTH = canvas.width;
+  var HEIGHT = canvas.height;
+  var r = Math.random();
 
+  function init() {
+    return setInterval(draw, 10);
+  }
 
+  function circle(x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI*2, true);
+    ctx.closePath();
+    ctx.fill();
+  }
+  function rect(x,y,w,h) {
+    ctx.beginPath();
+    ctx.rect(x,y,w,h);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  }
+
+  function draw() {
+    clear();
+    circle(x, y, 10);
+
+    if (x + dx > WIDTH || x + dx < 0)
+      dx = -dx;
+    if (y + dy > HEIGHT || y + dy < 0)
+      dy = -dy;
+
+    x += dx;
+    y += dy;
+
+  }
+
+  init();
+
+})();
 //draw a circle
 
-ctx.beginPath();
-ctx.arc(75, 75, 10, 0, Math.PI*2, true);
-ctx.closePath();
-ctx.fillStyle = "#d33"
-ctx.fill();
-
 //the rectangle is half transparent
-ctx.fillStyle = "#aaa"
-ctx.beginPath();
-ctx.rect(15, 150, 120, 30);
-ctx.closePath();
-ctx.fill();
+// ctx.fillStyle = "#aaa"
+// ctx.beginPath();
+// ctx.rect(15, 150, 120, 30);
+// ctx.closePath();
+// ctx.fill();
 
 
-var x = 150;
-var y = 150;
-var dx = 2;
-var dy = 2;
-
-function init() {
-  return setInterval(draw, 10);
-}
