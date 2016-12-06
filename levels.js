740349943cd47f5b28944afd215885dff859fab3
patricksimonian <patricksimonian@gmail.com>
2016-12-05 19:46:08 -0800

    added comments to levels.js explaining the functionality of the level parser

--- levels.js
+++ levels.js
@@ -1,7 +1,17 @@
 
+// this level parser takes in a comma seperated string and created an object dependant
+// on the value of each character in the string
+//==================LEGEND======================
+// ',':
+// is a point where the array is split and emulates a row in the 2d grid of bricks
+//----------------------------------------------
+// '=':
+// is a brick of 25 height and 50 width
+//----------------------------------------------
+// ' ':
+// is an empty space of 25 height and 50 width
+//----------------------------------------------
 
-
-//returns array of objects of level
 function levelParser(level) {
   //split elements by their commas
   var array = level.split(",");
