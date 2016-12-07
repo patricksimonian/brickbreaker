2b706802371535c25a72bb2b1d601042fa710d60
patricksimonian <patricksimonian@gmail.com>
2016-12-07 10:24:45 -0800

    added some logic for changing ball x velocity dependant on location where it struck the paddle (needs work)

--- canvas.js
+++ canvas.js
@@ -1,11 +1,12 @@
 
 (function initialize() {
+  var level1 = "===========,== == ====, == == == , === === =";
   var canvas = document.getElementById('game');
   var ctx = canvas.getContext("2d");
   canvas.height = 500;
   canvas.width = 500;
-  var level1 = "===========,== == ====, == == == , === === =";
-
+  var HEIGHT = canvas.height;
+  var WIDTH = canvas.width;
 
   //for ball
   var x = 150;
@@ -14,16 +15,18 @@
   var dx = 2;
   var dy = 4;
 
+  //paddle
+  var rx = 50;
+  var ry = 450;
+  var rWidth = 100;
+  var rHeight = 25;
+  var move = null;
+
   //get current level
   var game = new LevelParser().buildLevel(level1);
   var bricks = game.getCurrentLevel();
 
-  //rectanlge x movement
-  var rx = 50;
-  var ry = 450;
-  var move = null;
-  var WIDTH = canvas.width;
-  var HEIGHT = canvas.height;
+  //event listener for moving paddle
   document.addEventListener('keydown', function(e) {
     if(e.key === 'ArrowLeft') {
       move = 'left';
@@ -32,21 +35,8 @@
       move = 'right';
     }
   })
-  document.addEventListener('keyup', function(e) {
-    if(e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-      move = null;
-    }
-  });
-
-  function init() {
-    return setInterval(draw, 500  );
-  }
-
-  function clear() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  }
-
-  function moveRect() {
+  //logic used to currently move the paddle
+  function movePaddle() {
     if(move === 'left') {
       rx+= -5;
     } else if(move === 'right') {
@@ -60,6 +50,23 @@
     }
   }
 
+  document.addEventListener('keyup', function(e) {
+    if(e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+      move = null;
+    }
+  });
+
+  // set game loop
+  function init() {
+    return setInterval(draw, 10);
+  }
+
+  //clear canvas to emulate animation
+  function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  }
+  //current logic to check for brick collisions
+  //is being phased out
   function checkHit(genBricks) {
     var rowheight = 25 + r - 2;
     var colwidth = 50;
@@ -79,38 +86,41 @@
   }
 
   function draw() {
-    //current angle of ball
-    let theta = Math.atan((dy/dx)) + (.5 * Math.PI);
-    moveRect();
+    movePaddle();
     clear();
     circle(ctx, x, y, r);
     //paddle
-    rect(ctx, rx, ry, 100, 25);
+    rect(ctx, rx, ry, rWidth, rHeight);
     checkHit(genBricks);
     genBricks(ctx, game.getCurrentLevel());
-    willCollideWith({name: 'paddle', posY: ry, posX: rx, dimY: 25, dimX: 100},{name: 'ball', posY: y, posX: x, radius: r, dx: dx, dy: dy});
-    // if(bricks.every(function (elm) {
-    //   if(elm.isHit === true || elm.isHit === null) {
-    //     return true
-    //   }})) {
-    //   console.log("game over!")
-    // }
+    // willCollideWith(
+    //   {name: 'paddle', posY: ry, posX: rx, dimY: rHeight, dimX: rWidth},
+    //   {name: 'ball', posY: y, posX: x, radius: r, dx: dx, dy: dy}
+    // );
+
     //collision against wall
-    if (x + dx > (WIDTH - (r - 2)) || x + dy < (r - 2)){
+    if (x + dx > (WIDTH - r) || x + dy < r){
       dx = -dx;
     }
-    if (y + dy > (HEIGHT - (r - 2)) || y + dy < (r - 2)) {
+    if (y + dy > (HEIGHT - r) || y + dy < r) {
       dy = -dy;
     }
-    //collision against paddle
-    if (y + dy > (450 - (r  - 2)) && (x > rx && x < rx + 100)) {
-
+    //collision against paddle  ** need to add logic here to change x component of ball
+    //depending on where it hit the paddle
+    if (y + dy > (ry - r) && (x > rx && x < rx + rWidth)) {
+      if(x > rx && x < (rx + (.25 * rWidth))){
+        dx = dx > 0 ? -3 : 2;
+      } else if(x > (rx + (.75 * rWidth)) && x < (rx + rWidth)) {
+        dx = dx > 0 ? 3 : 2;
+      } else {
+        dx = dx > 0 ? -dx : dx;
+      }
       dy = -dy;
     }
     x += dx;
     y += dy;
   }
-
+  //start game
   init();
 
 })();
